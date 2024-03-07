0ea8255d98d4cd9e99807f3e268c0889abf03512
Ula <ula.malin35@gmail.com>
2024-03-07 01:15:27 +0100

    Changed docstrings and deleted main section

--- tasks/matrixes/app_6_zbior_10.py
+++ tasks/matrixes/app_6_zbior_10.py
@@ -19,14 +19,21 @@ def get_size_of_matrix_until_predicate(
         predicate: Callable[[int, int], bool] = lambda a, b: abs(a - (b // 2)) >= 3) \
         -> tuple[int, int]:
     """
-    Generates the size of a matrix until a specified predicate is satisfied
-    :param r_min: int the minimum number of rows/columns in the matrix
-    :param r_max: int the maximum number of rows/columns in the matrix
-    :param predicate: Callable[[int, int], bool] a function that takes two integers and returns a boolea. The predicates
-    determines whether the generated size satisfies the condition. Defaults to check if the absolute difference between
-    the number of rows and half the number of columns is at least 3
-    :return tuple[int,int]: a tuple containing the number of rows and columns for matrix
+    Generates the size of a matrix until a specified predicate is satisfied.
+
+    Parameters:
+        r_min (int): The minimum number of rows/columns in the matrix. Defaults to 2.
+        r_max (int): The maximum number of rows/columns in the matrix. Defaults to 20.
+        predicate (Callable[[int, int], bool]): A function that takes two integers and returns a boolean.
+            It determines whether the generated size satisfies the condition. Defaults to check if the
+             absolute difference between the number of rows and half the number of columns is at least 3.
+
+    Returns:
+        tuple[int,int]: A tuple containing the number of rows and columns for the matrix.
+    Raises:
+        ValueError: if r_min higher than r_max.
     """
+
     if r_min > r_max:
         raise ValueError('Incorrect range')
     while True:
@@ -38,34 +45,46 @@ def get_size_of_matrix_until_predicate(
 
 def generate_matrix(rows: int, columns: int, r_min: int = 30, r_max: int = 100) -> list[list[int]]:
     """
-    Generates a two-dimensional array with random integer elements
-    :param rows: int the number of rows in the matrix
-    :param columns: int the number of columns in the matrix
-    :param r_min: int the minimum value for the randomly generated elements. Defaults to 30
-    :param r_max: int the maximum value for the randomly generated elements. Defaults to 100
-    :return list[list[int]]: a two-dimensional list representing the matrix
+    Generates a two-dimensional array with random integer elements.
+
+    Parameters:
+        rows (int): The number of rows in the matrix.
+        columns (int): The number of columns in the matrix.
+        r_min (int): The minimum value for the randomly generated elements. Defaults to 30.
+        r_max (int): The maximum value for the randomly generated elements. Defaults to 100.
+
+    Returns:
+        list[list[int]]: A two-dimensional list representing the matrix.
     """
 
     return [[random.randint(r_min, r_max) for _ in range(columns)] for _ in range(rows)]
 
 
-def calculate_row_average(matrix: list[list[int]]) -> list[float]:
+def calculate_rows_average(matrix: list[list[int]]) -> list[float]:
     """
-    Calculates the average value of elements in each row of the matrix
+    Calculates the average value of elements in each row of the matrix.
 
-    :param matrix: list[list[int]] input matrix
-    :return list[float]: a list containing the average value of elements for each row in the matrix
+    Parameters:
+        matrix (list[list[int]]): Input matrix.
+
+    Returns:
+        list[float]: A list containing the average value of elements for each row in the matrix.
     """
+
     return [sum(n for n in row) / len(row) for row in matrix]
 
 
-def get_indexes_where_average_between(averages: list[float]) -> list[int]:
+def find_rows_with_averages_between_neighbours(averages: list[float]) -> list[int]:
     """
-    Finds the indexes of rows whose average values are between the averages of their neighboring rows
+    Finds the indexes of rows whose average values are between the averages of their neighboring rows.
 
-    :param averages: list[float] a list containing the average value of elements for each row of the matrix
-    :return: list[int] a list of indexes of rows whose average values meet the specified condition
+    Args:
+        averages (list[float]): A list containing the average value of elements for each row of the matrix.
+
+    Returns:
+        list[int]: A list of indexes of rows whose average values meet the specified condition.
     """
+
     matching_indexes = []
     for i in range(1, len(averages) - 1):
         if averages[i - 1] <= averages[i] <= averages[i + 1] or averages[i - 1] >= averages[i] >= averages[i + 1]:
@@ -73,34 +92,16 @@ def get_indexes_where_average_between(averages: list[float]) -> list[int]:
     return matching_indexes
 
 
-def zero_out_rows_not_matchin_indexes(matrix: list[list[int]], indexes: list[int]) -> list[list[int]]:
+def zero_out_rows_not_matching_indexes(matrix: list[list[int]], indexes: list[int]) -> list[list[int]]:
     """
-    Zeros out rows in the matrix that do not match the specified indexes
+    Zeros out rows in the matrix that do not match the specified indexes.
 
-    :param matrix: list[list[int]] input matrix
-    :param indexes: list[int] a list of indexes of rows to keep unchanged
-    :return: list[list[int]] the modified matrix with rows not matching the indexes zeroed out
+    Parameters:
+        matrix (list[list[int]]): Input matrix.
+        indexes (list[int]): A list of indexes of rows to keep unchanged.
+
+    Returns:
+        list[list[int]]: The modified matrix with rows not matching the indexes zeroed out.
     """
+
     return [matrix[i] if i in indexes else [0] * len(matrix[i]) for i in range(len(matrix))]
-
-
-def main() -> None:
-    size = get_size_of_matrix_until_predicate()
-    matrix = [[90, 71, 81, 93],
-              [75, 43, 39, 80],
-              [34, 86, 49, 76],
-              [36, 99, 46, 99],
-              [49, 38, 56, 47],
-              [36, 38, 68, 83],
-              [30, 31, 79, 73],
-              [45, 50, 37, 77]]
-    rows_average = calculate_row_average(matrix)
-    print(rows_average)
-    matching_indexes = get_indexes_where_average_between(rows_average)
-    modified_matrix = zero_out_rows_not_matchin_indexes(matrix, matching_indexes)
-    print(modified_matrix)
-
-
-if __name__ == '__main__':
-    main()
-# DONE
