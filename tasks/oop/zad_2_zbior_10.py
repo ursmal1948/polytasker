1ede38c0921700b37dae78b0f1272f9212d827ed
Ula <ula.malin35@gmail.com>
2024-03-19 20:10:14 +0100

    improvement of the task

--- tasks/oop/zad_2_zbior_10.py
+++ tasks/oop/zad_2_zbior_10.py
@@ -40,46 +40,46 @@ from collections import defaultdict
 
 class Lists:
     def __init__(self, filename: str):
-        self.smallest = []
-        self.middle = []
-        self.highest = []
+        self.smallest_numbers = []
+        self.middle_numbers = []
+        self.highest_numbers = []
 
         with open(filename, 'r') as f:
             lines = [line.strip() for line in f.readlines()]
             numbers = [[int(n) for n in nums.split(';')] for nums in lines]
-            self.smallest = [min(subnumbers) for subnumbers in numbers]
-            self.middle = [sorted(subnumbers)[1] for subnumbers in numbers]
-            self.highest = [max(subnumbers) for subnumbers in numbers]
+            self.smallest_numbers = [min(subnumbers) for subnumbers in numbers]
+            self.middle_numbers = [sorted(subnumbers)[1] for subnumbers in numbers]
+            self.highest_numbers = [max(subnumbers) for subnumbers in numbers]
 
     def is_file_perfect(self) -> bool | tuple[bool, dict[int, list[int]]]:
         numbers_to_delete = {0: [], 1: []}
 
-        for i in range(len(self.smallest)):
-            for j in range(len(self.middle)):
-                if not self.smallest[i] < self.middle[j]:
-                    numbers_to_delete[0].append(self.smallest[i])
+        for i in range(len(self.smallest_numbers)):
+            for j in range(len(self.middle_numbers)):
+                if not self.smallest_numbers[i] < self.middle_numbers[j]:
+                    numbers_to_delete[0].append(self.smallest_numbers[i])
 
-        for i in range(len(self.middle)):
-            for j in range(len(self.highest)):
-                if not self.middle[i] < self.highest[j]:
-                    numbers_to_delete[1].append(self.middle[i])
+        for i in range(len(self.middle_numbers)):
+            for j in range(len(self.highest_numbers)):
+                if not self.middle_numbers[i] < self.highest_numbers[j]:
+                    numbers_to_delete[1].append(self.middle_numbers[i])
         values = [n for n in numbers_to_delete.values() if n]
         return True if not values else (False, numbers_to_delete)
 
     def get_diff_between(self,
-                         function_fn_1: Callable[[list[int]], int],
-                         function_fn_2: Callable[[list[int]], int]) \
+                         finisher_fn_1: Callable[[list[int]], int],
+                         finisher_fn_2: Callable[[list[int]], int]) \
             -> int:
-        return function_fn_1(self.smallest) - function_fn_2(self.middle)
+        return finisher_fn_1(self.smallest_numbers) - finisher_fn_2(self.middle_numbers)
 
-    def count_elements_divisible_by_number(self, n: int):
+    def count_elements_divisible_by(self, n: int):
         if n == 0:
             raise ValueError('Can not divide by 0')
         nn = abs(n) if n < 0 else n
-        return sum(1 for num in self.highest if num % nn == 0)
+        return sum(1 for num in self.highest_numbers if num % nn == 0)
 
     @staticmethod
-    def get_length_nondecreasing_sequence(numbers: list[int]):
+    def get_length_of_nondecreasing_sequence(numbers: list[int]):
         if numbers[1] < numbers[0]:
             return 0
         count = 1
@@ -90,11 +90,10 @@ class Lists:
                 break
         return count
 
-    # min max lub tez avg niemalejaca dlugosc ciagu
     def find_extreme_non_decreasing_sequence(self, extreme_fn: Callable[[list[int]], int]) -> None:
-        length_l1 = Lists.get_length_nondecreasing_sequence(self.smallest)
-        length_l2 = Lists.get_length_nondecreasing_sequence(self.middle)
-        length_l3 = Lists.get_length_nondecreasing_sequence(self.highest)
+        length_l1 = Lists.get_length_of_nondecreasing_sequence(self.smallest_numbers)
+        length_l2 = Lists.get_length_of_nondecreasing_sequence(self.middle_numbers)
+        length_l3 = Lists.get_length_of_nondecreasing_sequence(self.highest_numbers)
         extreme_value = extreme_fn([length_l1, length_l2, length_l3])
 
         if extreme_value == length_l1:
@@ -104,35 +103,34 @@ class Lists:
         else:
             print('THIRD LIST')
 
-    def find_min_max_index_difference(self) -> tuple[int, int]:
+    def find_extreme_difference(self, extreme_fn: Callable[[list[int]], int]) -> int | list[int]:
+        sorted_smallest = sorted(self.smallest_numbers, reverse=True)
+        sorted_middle = sorted(self.middle_numbers, reverse=True)
+        sorted_highest = sorted(self.highest_numbers, reverse=True)
 
-        sorted_smallest = sorted(self.smallest, reverse=True)
-        sorted_middle = sorted(self.middle, reverse=True)
-        sorted_highest = sorted(self.highest, reverse=True)
-
-        grouped_by_diff = defaultdict(int)
-        for i in range(0, len(self.smallest)):
+        grouped_by_diff = defaultdict(list)
+        for i in range(0, len(self.smallest_numbers)):
             diff_1 = abs(sorted_smallest[i] - sorted_middle[i])
             diff_2 = abs(sorted_smallest[i] - sorted_highest[i])
             diff_3 = abs(sorted_middle[i] - sorted_highest[i])
             diff = diff_1 + diff_2 + diff_3
-            grouped_by_diff[i] = diff
+            grouped_by_diff[diff].append(i)
 
-        smallest_diff = min(grouped_by_diff.items(), key=lambda e: e[1])[0]  # index 4: 92
-        highest_diff = max(grouped_by_diff.items(), key=lambda e: e[1])[0]  # index 0: 824
-        return smallest_diff, highest_diff
+        extreme_diff = extreme_fn(list(grouped_by_diff.keys()))
+        result = grouped_by_diff[extreme_diff]
+        return result[0] if len(result) == 1 else result
 
 
 def main() -> None:
     filename = 'data/app_2_numbers.txt'
     lists = Lists(filename)
-    print(lists.smallest)
-    print(lists.middle)
-    print(lists.highest)
+    print(lists.smallest_numbers)
+    print(lists.middle_numbers)
+    print(lists.highest_numbers)
     nums = [1, 0, 1, 1, 1, 0]
-    print(lists.get_length_nondecreasing_sequence(nums))
+    print(lists.get_length_of_nondecreasing_sequence(nums))
     print(lists.find_extreme_non_decreasing_sequence(lambda numbers: max(numbers)))
-    print(lists.find_min_max_index_difference())
+    print(lists.find_extreme_difference(lambda numbers: min(numbers)))
 
 
 if __name__ == '__main__':
