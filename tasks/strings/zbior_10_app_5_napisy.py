f81fe77a14961d38337f5928bda6d15d93f89a0d
Ula <ula.malin35@gmail.com>
2024-03-23 14:40:05 +0100

    Refactoring of code in strings package: added documentation, example usage in main function + translation of tasks' content to english

--- tasks/strings/zbior_10_app_5_napisy.py
+++ tasks/strings/zbior_10_app_5_napisy.py
@@ -2,22 +2,49 @@ import re
 from tasks.common_functions import get_string_until
 
 """
-Napisz program, w którym pobierasz od użytkownika napis, dopóki nie będzie posiadał
-na przemian dużej litery oraz cyfry. Przykładem napisu, który spełnia to kryterium, jest A9B3C7.
-Należy na podstawie pobranego napisu wygenerować nowy napis, w którym zastąpisz cyfrę liczbami,
-które są dzielnikami kodu ASCII znaku poprzedzającego cyfrę większymi od tej cyfry. Przykładowo dla napisu B3 wynikiem
-jest B611223366, ponieważ litera B ma kod ASCII 66. Dzielnikami liczby 66 są: 1, 2, 3, 6, 11, 22, 33, 66.
- Dzielniki większe od 3 to 6, 11, 22, 33, 66 i to one zastępują cyfrę 3 w napisie.
+Write a program where you prompt the user for a string until it contains alternating uppercase letters and digits.
+An example of a string meeting this criterion is A9B3C7.
+Based on the entered string, generate a new string where each digit is replaced by numbers that are divisors
+of the ASCII code of the character preceding the digit and greater than the digit.
+For example, for the string B3, the result is B611223366, because the letter B has an ASCII code of 66.
+The divisors of 66 are: 1, 2, 3, 6, 11, 22, 33, 66.
+Divisors greater than 3 are 6, 11, 22, 33, 66, and they replace the digit 3 in the string.
 """
 
 
 def does_string_match_regex(text: str, regex: str = r'^([A-Z]\d)+$') -> bool:
+    """
+    Check if the string matches a given regular expression pattern.
+
+    Parameters:
+        text (str): The string to be checked.
+        regex (str, optional): The regular expression pattern.
+
+    Returns:
+        bool: True if the string matches the pattern, False otherwise.
+    """
+
     if not re.match(regex, text):
         return False
     return True
 
 
 def get_divisors(n: int) -> list[int] | float:
+    """
+    Get the divisors of a given integer.
+
+    Parameters:
+        n (int): The integer for which divisors are to be found.
+
+    Returns:
+        list[int] | float: A list of divisors of the integer.
+            Returns infinity if the input is 0.
+
+    Example:
+       Input: n = 12
+        Output: [1, 2, 3, 4, 6, 12]
+    """
+
     if n == 0:
         return float('inf')
     nn = abs(n) if n < 0 else n
@@ -37,6 +64,20 @@ def get_divisors(n: int) -> list[int] | float:
 
 
 def replace_digits_with_divisors(text: str) -> str:
+    """
+    Replace digits in a string with divisors greater than the ASCII value of the preceding character.
+
+    Parameters:
+        text (str): The input string containing alternating uppercase letters and digits.
+
+    Returns:
+        str: The modified string where digits are replaced with their respective divisors.
+
+    Example:
+        Input: B9D5
+        Output: B11223366D173468
+    """
+
     items = []
     for i in range(1, len(text), 2):
         digit = int(text[i])
@@ -45,3 +86,13 @@ def replace_digits_with_divisors(text: str) -> str:
         divisors = [d for d in get_divisors(char_code) if d > digit]
         items.extend([char] + divisors)
     return ''.join(str(i) for i in items)
+
+
+def main() -> None:
+    text = get_string_until("Get string", lambda string: does_string_match_regex(string))
+    modified_text = replace_digits_with_divisors(text)
+    print(f'Modified text: {modified_text}')
+
+
+if __name__ == '__main__':
+    main()
