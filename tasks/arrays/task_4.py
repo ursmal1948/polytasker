2245ca3f3e294c63b2338e65cf24782ba9c16047
Ula <ula.malin35@gmail.com>
2024-04-02 17:57:23 +0200

    Implemented tests for task_4.py and task_5.py from arrays package

--- tasks/arrays/task_4.py
+++ tasks/arrays/task_4.py
@@ -2,6 +2,8 @@ import random
 from typing import Callable
 from tasks.common_functions import rand_number
 from algohub.algorithms.numbers.primes import is_prime_basic
+from algohub.algorithms.numbers.divisors import count_divisors
+from algohub.algorithms.numbers.digits import sum_digits
 
 """
 Prepare a one-dimensional array of integers. The dimension of the array
@@ -17,7 +19,8 @@ as 5, 4, 3, and 2.
 
 def generate_n_numbers_until_predicate(size: int, r_min: int = 100, r_max: int = 200,
 
-                                       predicate_fn: Callable[[int], bool] = lambda n: n % 2 == 1):
+                                       predicate_fn: Callable[[int], bool] = lambda n: count_divisors(
+                                           sum_digits(n)) - 2 == 2):
     """
     Generate a list of numbers until the specified predicate function is satisfied.
 
@@ -25,7 +28,9 @@ def generate_n_numbers_until_predicate(size: int, r_min: int = 100, r_max: int =
         size (int): The size of the list to be generated.
         r_min (int): The minimum value for number generation (default is 100).
         r_max (int): The maximum value for number generation (default is 200).
-        predicate_fn (Callable[[int], bool]): The predicate function to satisfy (default is odd numbers).
+        predicate_fn (Callable[[int], bool]): The predicate function to satisfy (default is
+         sum of digits that has only 2 divisors).
+        (excluding 1 and itself)).
 
     Returns:
         list[int]: A list of numbers satisfying the predicate function.
@@ -40,6 +45,7 @@ def generate_n_numbers_until_predicate(size: int, r_min: int = 100, r_max: int =
 
     if r_min > r_max:
         raise ValueError('Range is not correct')
+
     numbers = []
     while size > len(numbers):
         drawn_number = random.randint(r_min, r_max)
@@ -48,9 +54,29 @@ def generate_n_numbers_until_predicate(size: int, r_min: int = 100, r_max: int =
     return numbers
 
 
+def get_highest_n_elements(numbers: list[int], count: int) -> list[int]:
+    """
+    Get the highest n elements from a sorted list.
+
+    Parameters:
+        numbers (list[int]): The list of numbers.
+        count (int): The number of highest elements to retrieve.
+
+    Returns:
+        list[int]: The highest n elements from the list.
+
+    Example:
+        Input: numbers = [1, 2, 3, 4, 5, 6], count = 4
+        Output: [3,4,5,6]
+    """
+
+    return numbers[-count:]
+
+
 def process_n_extreme_elements(
-        numbers: list[int], count: int, extreme_fn: Callable[[list[int], int], list[int]],
-        finisher_fn: Callable[[list[int]], int]
+        numbers: list[int], count: int,
+        extreme_fn: Callable[[list[int], int], list[int]] = lambda nums, count: get_highest_n_elements(nums, count),
+        finisher_fn: Callable[[list[int]], int] = lambda nums: sum(nums)
 ) -> int:
     """
     Calculate the sum of the n extreme elements in a list.
@@ -68,67 +94,31 @@ def process_n_extreme_elements(
         ValueError: If there are not enough values in the list.
 
     Example:
-        Input: numbers = [10,20,30,40,50,60], count = 3, extreme_fn = lambda nums, count: get_highest(nums, count)
+        Input: numbers = [10,20,30,40,50,60], count = 3, extreme_fn = lambda nums, count: get_highest_n_elements(nums, count)
                finisher_fn = lambda nums: sum(nums)
         Output: 150
     """
 
     if count > len(numbers):
-        raise ValueError('Not enough values')
+        raise ValueError('Not enough numbers in a list')
     unique_numbers = set(numbers)
     sorted_numbers = sorted(unique_numbers)
     extreme_numbers = extreme_fn(sorted_numbers, count)
     return finisher_fn(extreme_numbers)
 
 
-def calculate_digits_sum(n: int) -> int:
-    """
-    Calculate the sum of digits of a number.
-
-    Parameters:
-        n (int): The number to calculate the sum of its digits.
-
-    Returns:
-        int: The sum of the digits of the number.
-
-    Example:
-        Input: 25024
-        Output: 13
-    """
-
-    nn = abs(n) if n < 0 else n
-    return sum(int(d) for d in str(nn))
-
-
-def get_highest_n_elements(numbers: list[int], count: int) -> list[int]:
-    """
-    Get the highest n elements from a list.
-
-    Parameters:
-        numbers (list[int]): The list of numbers.
-        count (int): The number of highest elements to retrieve.
-
-    Returns:
-        list[int]: The highest n elements from the list.
-
-    Example:
-        Input: numbers = [1, 2, 3, 4, 5, 6], count = 4
-        Output: [3,4,5,6]
-    """
-
-    return numbers[-count:]
-
-
 def main() -> None:
     array_length = rand_number(10, 100)
     print(f'Array length: {array_length}')
+    array_length = 10
     numbers = generate_n_numbers_until_predicate(size=array_length,
-                                                 predicate_fn=lambda n: is_prime_basic(calculate_digits_sum(n)))
+                                                 predicate_fn=lambda n: is_prime_basic(sum_digits(n)))
     print(f'Numbers: {numbers}')
     sum_of_extreme_elements = process_n_extreme_elements(numbers, 4,
                                                          lambda nums, count: get_highest_n_elements(nums, count),
                                                          lambda nums: sum(nums))
     print(f'Sum of extreme elements: {sum_of_extreme_elements}')
+    print(get_highest_n_elements(numbers, 4))
 
 
 if __name__ == '__main__':
---
+++ tests/arrays/test_task_5.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import mock_open, patch
+from tasks.arrays.task_5 import read_data_from_file, sort_items
+
+
+class TestTask5Functions(unittest.TestCase):
+    test_cases = [
+        ([12, 5, 8, 0, 3], [0, 3, 5, 8, 12]),
+        ([5], [5])
+    ]
+
+    @patch("builtins.open", new_callable=mock_open, read_data="10.0\n12.8\n17.9\n8.2\n15.0\n")
+    def test_read_data_from_file(self, mock_open):
+        result = read_data_from_file("test_file.txt")
+        self.assertEqual(result, [10.0, 12.8, 17.9, 8.2, 15.0])
+
+    def test_when_file_has_inorrect_extension(self):
+        with self.assertRaises(AttributeError) as e:
+            read_data_from_file("tests/arrays/data/test.text")
+        self.assertEqual(str(e.exception), "File has incorrect extension")
+
+    def test_when_file_not_found(self):
+        with self.assertRaises(FileNotFoundError) as e:
+            read_data_from_file("tests/arrays/data/testt.txt")
+        self.assertTrue(str(e.exception).startswith("File not found"))
+
+    def test_sort_items(self):
+        for test_case in self.test_cases:
+            numbers, expected_result = test_case
+            with self.subTest(test_case=test_case):
+                result = sort_items(numbers)
+                self.assertEqual(result, expected_result)
